c5e7ece5af72ab5ef8dbb27c682deca5bf806b9d
Mirba1 <toktosunuulumeerbek@gmail.com>
2024-02-01 04:18:24 -0600

    start of ramenhouse

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './assets/styles/main.css'
+import MainPage from "./pages/main/MainPage";
+
+function App() {
+  return (
+    <div>
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
